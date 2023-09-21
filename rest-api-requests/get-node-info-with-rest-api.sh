bb4460cceff83d1e5a67ad63a674750f421815b0
Isidro Lopez <islomar@gmail.com>
2023-09-21 22:29:49 +0200

    Include example to extract the status parameter

--- rest-api-requests/get-node-info-with-rest-api.sh
+++ rest-api-requests/get-node-info-with-rest-api.sh
@@ -1,1 +1,4 @@
 curl --request GET https://localhost:2746/api/v1/workflows/argo/suspend-template-j4txg --header "Content-Type: application/json" -d '{"nodeFieldSelector":"displayName=approve"}' --insecure | jq
+ 
+ # Extract the status parameter
+ #curl --request GET https://localhost:2746/api/v1/workflows/argo/suspend-template-j4txg --header "Content-Type: application/json" -d '{"nodeFieldSelector":"displayName=approve"}' --insecure | jq '.status.nodes[] | select(.outputs.parameters != null) | .outputs.parameters[].value'
